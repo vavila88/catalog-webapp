e652b3db17f4d9d08e8f3b92e4a6f56e8b844e02
Victor Avila <vic.avi88@gmail.com>
2019-04-01 10:42:42 -0600

    Add JS code for creating/removing elemnts from the edit page.

--- static/script.js
+++ static/script.js
@@ -16,4 +16,43 @@ function optionHandler() {
     }
 };
 
+function showCategoryOptions() {
+    // if the placeholder div exists, then we haven't selected the "change
+    // category" button and are doing so now. Replace the placeholder div with
+    // the category selection code.
+    if ($("div#edit-category").length) {
+        $.ajax({
+            url : "/api/v1/catalog/categories/JSON",
+            type : "GET",
+            dataType : "json",
+        }).done(function(result) {
+            let sel_hdr ='<select name="category_select" id="category_select" onchange="optionHandler()">' +
+                          '<option value="temp_cat">temp cat</option>';
+                            //{% for c in category_list%}
+            let opt_str = '';
+            // console.log(result['Category']);
+            for (var key in result['Category']) {
+                // console.log(result['Category'][key]);
+                opt_str = opt_str + '<option name="'+result['Category'][key]['name']+'">'+result['Category'][key]['name']+'</option>';
+            }
+                            //{% endfor %}
+            let sel_ftr = '<option value="new_cat">-- Add a new category --</option>'+
+                        '</select>';
+
+            $("div#edit-category").replaceWith(sel_hdr+opt_str+sel_ftr);
+            $('button#btn-change-cat').text('Keep Category');
+            // console.log(sel_hdr+opt_str+sel_ftr);
+        });
+    }
+    // Else, the div is gone and we clicked the "change category" button again,
+    // thereby indicating that we don't want to change the category and
+    // requiring us to hide the cat info.
+    else {
+        let div_str = '<div id="edit-category" style="display:none;"></div>'
+        $('select#category_select').replaceWith(div_str);
+        $('button#btn-change-cat').text('Change Category');
+
+    }
+};
+
 $(document).ready(function(){optionHandler();});
