3c04052722ebcbbaa69597617ad90ce2a24279d1
Victor Avila <vic.avi88@gmail.com>
2019-04-04 14:40:51 -0600

    Fix code to adhere to PEP8. One remaining invalid error.

--- application.py
+++ application.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
-app = Flask(__name__)
-
+from flask import (Flask, render_template, request, redirect,
+                   jsonify, url_for, flash)
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 from model import Base, User, Item, Category
 
 # Imports to implement anti-forgery state tokens
 from flask import session as login_session
-import random, string
-
 # OAUTH imports
 
-#Creates a flow object from the client secrets JSON file. This JSON stores the
+# Creates a flow object from the client secrets JSON file. This JSON stores the
 # client id, client secret, and other OAuth2 params
 from oauth2client.client import flow_from_clientsecrets
-#used when we run into an error when we try to exchange OTC for an auth token
+# used when we run into an error when we try to exchange OTC for an auth token
 from oauth2client.client import FlowExchangeError
 # comprehensive http client lib from python
 import httplib2
 # python json lib for creation/manipulation of json data
 import json
+import random
+import string
 # converts the response from a function into a real http response
 from flask import make_response
 # similar to urllib2, but with improvements
 import requests
 
-#Connect to Database and create database session
+app = Flask(__name__)
+
+# Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db?check_same_thread=False')
 Base.metadata.bind = engine
 
@@ -35,7 +36,8 @@ session = DBSession()
 
 # extract the client_id and secrets from our secrets file.
 CLIENT_ID = \
-    json.loads(open('client_secret.json','r').read())['web']['client_id']
+    json.loads(open('client_secret.json', 'r').read())['web']['client_id']
+
 
 @app.route('/')
 @app.route('/catalog')
@@ -49,7 +51,7 @@ def index():
     # for i in latest_items:
     #     print(i.slug)
     return render_template('catalog.html', category_list=category_list,
-        items=latest_items)
+                           items=latest_items)
 
 
 @app.route('/catalog/<cat_slug>/item')
@@ -68,7 +70,7 @@ def show_all_cat_items(cat_slug):
     return render_template('category.html', items=items, category=category)
 
 
-@app.route('/catalog/<cat_slug>/delete', methods=['GET','POST'])
+@app.route('/catalog/<cat_slug>/delete', methods=['GET', 'POST'])
 def delete_cat(cat_slug):
     """
     delete_cat - delete the specified category. This will also delete all
@@ -105,7 +107,6 @@ def delete_cat(cat_slug):
         return redirect(url_for('index'))
 
 
-
 @app.route('/catalog/item/<item_slug>')
 def show_cat_item(item_slug):
     """
@@ -122,7 +123,7 @@ def show_cat_item(item_slug):
         return redirect(url_for('index'))
 
 
-@app.route('/catalog/item/<item_slug>/delete', methods=['GET','POST'])
+@app.route('/catalog/item/<item_slug>/delete', methods=['GET', 'POST'])
 def delete_cat_item(item_slug):
     """
     delete_cat_item - deletes the item speficied by the slug
@@ -138,7 +139,7 @@ def delete_cat_item(item_slug):
         except:
             flash('Error fulfilling request.')
             return redirect(url_for('index'))
-    elif request.method =='POST':
+    elif request.method == 'POST':
         try:
             item = session.query(Item).filter_by(slug=item_slug).one()
         except:
@@ -146,12 +147,11 @@ def delete_cat_item(item_slug):
             return redirect(url_for('index'))
         session.delete(item)
         session.commit()
-        flash('Item %s successfully deleted from the catalog.'%item.title)
+        flash('Item %s successfully deleted from the catalog.' % item.title)
         return redirect(url_for('index'))
 
 
-
-@app.route('/catalog/item/<item_slug>/edit', methods=['GET','POST'])
+@app.route('/catalog/item/<item_slug>/edit', methods=['GET', 'POST'])
 def edit_cat_item(item_slug):
     """
     edit_cat_item - edit the item specified by the slug
@@ -167,13 +167,13 @@ def edit_cat_item(item_slug):
             item = session.query(Item).filter_by(slug=item_slug).one()
             category = session.query(Category).filter_by(id=item.cat_id).one()
             return render_template('edit_category_item.html', item=item,
-                    category=category)
+                                   category=category)
         except:
             flash('Error fulfilling request.')
             return redirect(url_for('index'))
     elif request.method == 'POST':
         for x in request.form:
-            print('%s : %s'%(x,request.form[x]))
+            print('%s : %s' % (x, request.form[x]))
 
         try:
             item = session.query(Item).filter_by(slug=item_slug).one()
@@ -187,11 +187,12 @@ def edit_cat_item(item_slug):
         # the item
         if 'category_select' in request.form:
             try:
-                cat = session.query(Category).filter_by(name=
-                        request.form['category_select']).one()
+                cat = session.query(Category).\
+                    filter_by(name=request.form['category_select']).one()
             except:
                 flash('Error fulfilling request.')
                 return redirect(url_for('index'))
+
             item.description = request.form['description']
             item.slug = gen_item_slug(cat.slug)
             item.cat_id = cat.id
@@ -204,8 +205,7 @@ def edit_cat_item(item_slug):
         return redirect(url_for('index'))
 
 
-
-@app.route('/catalog/item/new', methods=['GET','POST'])
+@app.route('/catalog/item/new', methods=['GET', 'POST'])
 def new_item():
     """
     new_item - Function that creates a new item. This function also creates a
@@ -217,7 +217,7 @@ def new_item():
         # dropdown
         category_list = session.query(Category).all()
         return render_template('new_category_item.html',
-                category_list=category_list)
+                               category_list=category_list)
     elif request.method == 'POST':
         # the data from the text fields are stored in the request.form dict
         # the name of the select element, use this along with the form elements
@@ -236,41 +236,39 @@ def new_item():
         try:
             try:
                 print('Retrieving the specified category - %s' % item_cat)
-                category = session.query(Category).filter_by(name=item_cat).one()
+                category = session.query(Category).filter_by(name=item_cat).\
+                    one()
             except:
                 new_cat_name = str(request.form['new_cat_title'])
-                print('Create a new category with name - {}'.format(new_cat_name))
+                print('Create a new category with name - {}'.
+                      format(new_cat_name))
                 new_category = Category(name=new_cat_name,
-                        slug=gen_cat_slug(new_cat_name))
+                                        slug=gen_cat_slug(new_cat_name))
                 session.add(new_category)
                 session.commit()
 
                 try:
-                    category = session.query(Category).filter_by(name=new_cat_name).\
-                            one()
+                    category = session.query(Category).\
+                        filter_by(name=new_cat_name).one()
                 except:
                     flash('Error fulfilling request.')
                     return redirect(url_for('index'))
 
             item_slug = gen_item_slug(category.slug)
-            print('Creating a new item with name - {}, cat_id - {}, desc -'\
-                    '"{}", slug - {}'.format(item_title, item_desc, \
-                        category.id, item_slug))
+            print('Creating a new item with name - {}, cat_id - {}, desc -'
+                  '"{}", slug - {}'.format(item_title, item_desc,
+                                           category.id, item_slug))
 
             newItem = Item(title=item_title, description=item_desc,
-                cat_id=category.id, slug=item_slug)
+                           cat_id=category.id, slug=item_slug)
 
             session.add(newItem)
             session.commit()
 
-            # categories = session.query(Category).order_by(asc(Category.name))
-            # latest_items = session.query(Item).order_by(desc(Item.id)).limit(5)
             return redirect(url_for('index'))
         except:
             flash('Unable to add duplicate item to the database: %s.' %
-                    item_title)
-            # categories = session.query(Category).order_by(asc(Category.name))
-            # latest_items = session.query(Item).order_by(desc(Item.id)).limit(5)
+                  item_title)
             return redirect(url_for('index'))
 
 
@@ -279,21 +277,21 @@ def gen_cat_slug(base):
     gen_cat_slug - Generates a slug for a category, namely, converts to all
     lowercase and replaces ' ' with '-'
     """
-    ret = base.lower().replace(' ','-')
-    print('slug for category %s - %s' %(base, ret))
+    ret = base.lower().replace(' ', '-')
+    print('slug for category %s - %s' % (base, ret))
     return ret
 
 
 def gen_item_slug(base):
     """
-    gen_item_slug - generates a slug out of the provided base string and 5 random
-    alphanumeric characters.
+    gen_item_slug - generates a slug out of the provided base string and 5
+    random alphanumeric characters.
     """
-    return base + '-' +''.join(random.SystemRandom().choice(
+    return base + '-' + ''.join(random.SystemRandom().choice(
         string.ascii_lowercase+string.digits) for x in xrange(5))
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """
     login - login handler used to retrieve google OAuth credentials
@@ -309,38 +307,40 @@ def login():
             host = request.headers['host']
             src_url = request.headers['referer']
 
-            redirect_path = src_url.replace(host,'')
+            redirect_path = src_url.replace(host, '')
             return redirect(redirect_path)
 
         # creates a pseudo-random alphanumeric string of 32 characters to act
         # as an anti-forgery token. This token is recreated every time '/login'
         # is hit
-        state = ''.join(random.choice(string.ascii_uppercase+string.digits) \
-                for x in xrange(32))
+        state = ''.join(random.choice(string.ascii_uppercase+string.digits)
+                        for x in xrange(32))
         # store this generated token to the session
         login_session['state'] = state
         # pass in the client ID provided by google
         return render_template('login.html', STATE=state, clientId=CLIENT_ID)
     # handle POST requests to this endpoint
     elif request.method == 'POST':
-        # verify that this request came from a user attempting to log in via the
-        # login page and not from some bot hitting this endpoint randomly
+        # verify that this request came from a user attempting to log in via
+        # the login page and not from some bot hitting this endpoint randomly
         if request.args.get('state') != login_session['state']:
             # respond that there was a mismatch
-            response = make_response(json.dumps('Invalid state parameter'), 401)
+            response = make_response(json.dumps('Invalid state parameter'),
+                                     401)
             response.headers['Content-Type'] = 'application/json'
             return response
 
         # consume the state provided for this particular login flow
         login_session['state'] = None
-        #get the query params from the body of the post
+        # get the query params from the body of the post
         code = request.data
-            # specify that this is the OTC flow that we'll be sending
+        # specify that this is the OTC flow that we'll be sending
 
         # exchange the oauth OTC for a long-term auth token
         try:
             # creats an oauth flow object, adds client secret key
-            oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+            oauth_flow = flow_from_clientsecrets('client_secret.json',
+                                                 scope='')
             # NOTE: despite the redirect URI not being used in this flow, it
             # MUST be set as follows, otherwise Flask throws a 401 -
             # unauthorized error. It does not need to be present in the html
@@ -349,8 +349,8 @@ def login():
             credentials = oauth_flow.step2_exchange(code)
         except FlowExchangeError:
             # respond that there was a mismatch
-            response = make_response(json.dumps('Failed to upgrade the '\
-                    'authorization code'), 401)
+            response = make_response(json.dumps('Failed to upgrade the '
+                                                'authorization code'), 401)
             response.headers['Content-Type'] = 'application/json'
             return response
 
@@ -368,7 +368,7 @@ def login():
         login_session['gplus_id'] = credentials.id_token['sub']
 
         userinfo_url = 'https://www.googleapis.com/oauth2/v2/userinfo'
-        params = {'access_token':login_session['access_token'], 'alt':'json'}
+        params = {'access_token': login_session['access_token'], 'alt': 'json'}
 
         # send the request and get the answer in JSON format
         answer = requests.get(userinfo_url, params=params)
@@ -382,13 +382,13 @@ def login():
         uid = getUserId(login_session['email'])
         if uid is None:
             # if we got nothing from the db when we pinged it with the provided
-            # email, then we know that there is no such user and we need to add a
-            # new one
+            # email, then we know that there is no such user and we need to add
+            # a new one
             print('Adding new user with email %', login_session['email'])
             uid = createUser(login_session)
 
-        # either after creating a new user or after verifying it's existence, set
-        # the session user_id variable
+        # either after creating a new user or after verifying it's existence,
+        # set the session user_id variable
         login_session['user_id'] = uid
         login_session['provider'] = 'google'
 
@@ -402,7 +402,7 @@ def login():
         #     'border-radius:150px;-webkit-border-radius: '\
         #     '150px;-moz-border-radius:150px;">'
 
-        flash('You are now logged in as %s.'%login_session['username'])
+        flash('You are now logged in as %s.' % login_session['username'])
         return output
 
 
@@ -461,10 +461,10 @@ def verify_access_token(credentials):
     None is returned upon successful verification of the token
     """
     access_token = credentials.access_token
-    url=('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}'\
-            .format(access_token))
+    url=('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}'.
+         format(access_token))
     h = httplib2.Http()
-    #send the GET request to google to verify
+    # send the GET request to google to verify
     results = json.loads(h.request(url, 'GET')[1])
 
     # if there are any errors, send an internal server error response
@@ -480,16 +480,16 @@ def verify_access_token(credentials):
     gplus_id = credentials.id_token['sub']
     if results['user_id'] != gplus_id:
         # respond that there was an id mismatch
-        response = make_response(json.dumps('Token ID does not match given '\
-                'user ID'), 401)
+        response = make_response(json.dumps('Token ID does not match given '
+                                 'user ID'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # check the client id returned in the results
     if results['issued_to'] != CLIENT_ID:
         # respond that there was an id mismatch
-        response = make_response(json.dumps('Token client ID does not match '\
-                'given client ID'), 401)
+        response = make_response(json.dumps('Token client ID does not match '
+                                 'given client ID'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -497,8 +497,8 @@ def verify_access_token(credentials):
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current User is already logged '\
-            'in'), 200)
+        response = make_response(json.dumps('Current User is already logged '
+                                 'in'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -509,8 +509,8 @@ def createUser(login_session):
     """
     Creates a new user in the DB using the session parameters
     """
-    newUser = User(uname=login_session['username'], email =
-            login_session['email'])
+    newUser = User(uname=login_session['username'],
+                   email=login_session['email'])
     session.add(newUser)
     session.commit()
 
@@ -558,18 +558,18 @@ def catalog_json():
         ret.append(c.serialize)
         # once we have the items, serialize them and add them to the dict.
         items = session.query(Item).filter_by(cat_id=c.id)
-        ret[-1]['Items']= [i.serialize for i in items]
-    return jsonify(Category = ret), 200
+        ret[-1]['Items'] = [i.serialize for i in items]
+    return jsonify(Category=ret), 200
 
 
 @app.route('/api/v1/catalog/category/JSON')
 def categories_json():
     """
-    categories_json - returns a JSON representation of all the categories in the
-    DB
+    categories_json - returns a JSON representation of all the categories in
+    the DB.
     """
     category_list = session.query(Category).order_by(asc(Category.name))
-    return jsonify(Category = [c.serialize for c in category_list])
+    return jsonify(Category=[c.serialize for c in category_list])
 
 
 @app.route('/api/v1/catalog/category/<cat_slug>/JSON')
@@ -607,6 +607,6 @@ def item_json(item_slug):
 
 
 if __name__ == '__main__':
-  app.secret_key = 'super_secret_key'
-  app.debug = True
-  app.run(host = '0.0.0.0', port = 8000)
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
